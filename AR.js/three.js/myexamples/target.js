2927324f78d80f72c55c401b3084b86e7cb5a605
madhipoo <55700173+madhipoo@users.noreply.github.com>
2020-05-21 01:44:12 +0800

    Add files via upload

--- AR.js/three.js/myexamples/target.js
+++ AR.js/three.js/myexamples/target.js
@@ -3,7 +3,7 @@ class Target {
 		this.test = test;
 		this.pos = pos;
 		this.id = 1;
-		this.mesh = new THREE.Mesh (new THREE.CylinderGeometry (8,8,3,20), 
+		this.mesh = new THREE.Mesh (new THREE.CylinderGeometry (3,3,3,20), 
 		    new THREE.MeshBasicMaterial ({color:'yellow'}));
 		this.mesh.position.copy (pos)
 		arWorldRoot.add (this.mesh);
